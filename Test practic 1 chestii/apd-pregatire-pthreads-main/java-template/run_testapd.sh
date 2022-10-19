2b19c5f517817c8437c20018f2a3d808725acf98
elena-bianca312 <56994824+elena-bianca312@users.noreply.github.com>
2022-10-19 19:08:52 +0300

    Delete Test practic 1 chestii directory

--- Test practic 1 chestii/apd-pregatire-pthreads-main/java-template/run_testapd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make
-
-java Testapd 10 3
-java Testapd 25 4
-java Testapd 3 8
-
-make clear
--- Test practic 1 chestii/apd-pregatire-pthreads-main/pthread-template/run_testapd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make
-
-./testapd 10 3
-./testapd 25 4
-./testapd 3 8
-
-make clear
